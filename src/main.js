9e0f8063358d14e4c2e7aeb92da3e33de2a81aa3
greta <greta.belegu@gmail.com>
2025-05-05 12:49:21 -0400

    Initial commit - add project files

---
+++ src/main.js
@@ -0,0 +1,7 @@
+
+gsap.from("h1", {
+  duration: 1,
+  y: -50,
+  opacity: 0,
+  ease: "power2.out"
+});
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",           // Include your HTML file
+    "./src/**/*.{js,jsx,ts,tsx}"  // Include JS files in 'src' folder (if any)
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
